e58088d2676da46d3153b080b1ceec60ac02e6fc
Michael Sun <sun.m710@gmail.com>
2024-07-10 22:54:14 -0400

    adding hooks context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,73 +1,103 @@
-import "./sign-in-form.styles.scss"
+import "./sign-in-form.styles.scss";
 
-import { useState } from "react"
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 
-import {signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword} from "../../utils/firebase/firebase.utils";
+import { UserContext } from "../../contexts/user.context";
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
-    email: '',
-    password: '',
-}
+  email: "",
+  password: "",
+};
 
 const SignInForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    const { email, password} = formFields;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
 
-    const resetFormFields = () => {
-        setFormFields(defaultFormFields);
+  const { setCurrentUser } = useContext(UserContext);
+
+  console.log("formfields", formFields);
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      const { user } = await signInWithGooglePopup();
+      await createUserDocumentFromAuth(user);
+    } catch {
+      return;
     }
+  };
 
-    const signInWithGoogle = async () => {
-        try {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-            const {user} = await signInWithGooglePopup();
-            await createUserDocumentFromAuth(user);
-        } catch{
-            return
-        }
+    try {
+      const { user } = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      setCurrentUser(user);
+      resetFormFields();
+    } catch (e) {
+      switch (e.code) {
+        case "auth/invalid-credential":
+          alert("Invalid credentials");
+          break;
+        default:
+          console.log(e);
+      }
     }
-    const handleSubmit = async(event) => {
-        event.preventDefault();
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
 
-        try {
-            const res = await signInAuthUserWithEmailAndPassword(email, password)
-            console.log(res);
-            resetFormFields()
-        } catch(e) {
-            switch(e.code) {
-                case 'auth/invalid-credential':
-                    alert('Invalid credentials');
-                    break;
-                default:
-                    console.log(e);
-            }
-        }
+  return (
+    <div className="sign-up-container">
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          onChange={handleChange}
+          name="email"
+          value={email}
+          required
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          onChange={handleChange}
+          name="password"
+          value={password}
+          required
+        />
 
-    }
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-        setFormFields({...formFields, [name]: value});
-    }
-
-    return (
-        <div className="sign-up-container">
-            <h2>Already have an account?</h2>
-            <span>Sign in with your email and password</span>
-            <form onSubmit={handleSubmit}>
-                <FormInput label="Email" type="email" onChange={handleChange} name="email" value={email} required/>
-                <FormInput label="Password" type="password" onChange={handleChange} name="password" value={password} required/>
-
-                {/* <button type="submit">Sign Up</button> */}
-                <div className="buttons-container">
-                    <Button buttonType='secondary' type="submit">Sign In</Button>
-                    <Button buttonType='primary' onClick={signInWithGoogle} type="button">Google Sign In</Button>
-                </div>
-            </form>
+        {/* <button type="submit">Sign Up</button> */}
+        <div className="buttons-container">
+          <Button buttonType="secondary" type="submit">
+            Sign In
+          </Button>
+          <Button buttonType="primary" onClick={signInWithGoogle} type="button">
+            Google Sign In
+          </Button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
-export default SignInForm
+export default SignInForm;
--- src/routes/authentication/authentication.jsx
+++ src/routes/authentication/authentication.jsx
@@ -1,18 +1,14 @@
-import './authentication.styles.scss'
+import "./authentication.styles.scss";
 
-import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
-import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const Authentication = () => {
-
-    return (
-        <div className='authentication-container'>
-            {/* <h1>sign in intact</h1> */}
-            {/* <button type="button" class="btn btn-primary" onClick={logGoogleUser}>Sign In Google popup</button> */}
-            {/* <button type="button" class="btn btn-primary" onClick={logGoogleRedirectUser}>Sign In Google redirect</button> */}
-            <SignInForm/>
-            <SignUpForm />
-        </div>
-    )
-}
-export default Authentication
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+export default Authentication;
