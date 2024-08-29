3dd0c3af545c7f8eab8e12135429317910495f55
Michael Sun <sun.m710@gmail.com>
2024-08-28 22:20:57 -0400

    updating Bootstrap for the homepage

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       id: 1,
       title: "movers",
       imgUrl:
-        "https://cdnep-uhaul-uhaulcom-global-p-001.azureedge.net/uhaulcom/cdn/Misc/Home/SixPack/moving-help.webp",
+        "https://cdnep-uhaul-uhaulcom-global-p-001.azureedge.net/uhaulcom/cdn/Misc/Home/SixPack/ubox.webp",
     },
     {
       id: 2,
@@ -20,7 +20,7 @@ const Home = () => {
       id: 3,
       title: "trucks",
       imgUrl:
-        "https://cdnep-uhaul-uhaulcom-global-p-001.azureedge.net/uhaulcom/cdn/Misc/Trucks/Home/truck-rentals-lg.webp",
+        "https://cdnep-uhaul-uhaulcom-global-p-001.azureedge.net/uhaulcom/cdn/Misc/Home/SixPack/moving-help.webp",
     },
     {
       id: 4,
@@ -31,10 +31,18 @@ const Home = () => {
   ];
   return (
     <div>
-      <Outlet/>
+      <Outlet />
+      <div class="container m-5">
+        <div class="card border-0">
+          <h3>Welcome to Michael's moving company!</h3>
+          <div class="card-title">We offer the service at the lowest price</div>
+          {/* <div class="card-body">This is some text within a card body.</div> */}
+        </div>
+      </div>
+      <div class="mb-4">What we offer:</div>
       <Directory categories={categories} />
     </div>
-  )
+  );
 };
 
 export default Home;
