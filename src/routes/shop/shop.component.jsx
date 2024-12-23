314b3d9365599736b5a42155248aa559dab6d773
Michael Sun <sun.m710@gmail.com>
2024-12-23 16:54:19 -0500

    adding custom logger

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -7,13 +7,13 @@ import Category from "../category/category.component";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategories } from "../../store/categories/category.action";
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      console.log("categoryMap from CategoriesProvider", categoryMap);
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(categoriesArray));
     };
     getCategoriesMap();
   }, []);
