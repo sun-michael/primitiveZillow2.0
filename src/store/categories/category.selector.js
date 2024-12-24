5ea34e20fca473e61a30e1bb61e9ea256dfe77c4
Michael Sun <sun.m710@gmail.com>
2024-12-24 10:25:46 -0500

    adding reselect for better resource utilization

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,22 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    console.log("selector 1 fired");
+    return categoriesSlice.categories;
+  }
+);
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    console.log("selector 2 fired");
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {});
+  }
+);
