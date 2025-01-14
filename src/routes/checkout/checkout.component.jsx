28c480c25b5d4777fb1dc729f5b05f91bcc97896
Michael Sun <sun.m710@gmail.com>
2025-01-14 16:53:23 -0500

    adding stripe for payment

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,6 +7,9 @@ import {
 } from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
@@ -35,6 +38,7 @@ const Checkout = () => {
         return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
       })}
       <span className="total"> Total: ${cartTotal}</span>
+      <PaymentForm />
     </div>
     // </div>
   );
